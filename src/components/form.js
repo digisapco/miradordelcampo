3e063f9061b36f5e1d90b687e7ac48dddd1cd9c3
klsierraj <klsierraj47@gmail.com>
2020-09-04 18:46:22 -0500

    presio

--- src/components/form.js
+++ src/components/form.js
@@ -150,7 +150,7 @@ const Userform = () => {
             <label>Acepto términos y políticas de privacidad.</label>
           </div>
           <div>
-            <input type="submit" />
+            <input className="boton" type="submit" />
           </div>
         </form>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import gym from "../../content/assets/gym.svg"
 import piscina from "../../content/assets/piscina.svg"
 import salon from "../../content/assets/salon.svg"
 import juegos from "../../content/assets/juegos.svg"
+import plano1 from "../../content/assets/precio.png"
 import AwesomeSlider from "react-awesome-slider"
 import withAutoplay from "react-awesome-slider/dist/autoplay"
 import "react-awesome-slider/dist/styles.css"
@@ -101,22 +102,29 @@ const BlogIndex = ({ data }, location) => {
       </div>
 
       <div className="section">
-        <div className="container">
-          <div className="caract">
-            <h2
-              style={{
-                color: "#84C226",
-              }}
-            >
-              CARACTERÍSTICAS
-            </h2>
+        <div className="containerlogos">
+          <div className="caract"></div>
+          <div className="logogrid">
+            <div className="logos">
+              <div className="titlelogo">
+                <h2
+                  style={{
+                    color: "#84C226",
+                  }}
+                >
+                  CARACTERÍSTICAS
+                </h2>
+              </div>
+              <img src={gym}></img>
+              <img src={piscina}></img>
+              <img src={salon}></img>
+              <img src={juegos}></img>
+            </div>
+            <div className="imagepiscina">
+              <Img fixed={data.piscina.childImageSharp.fixed} />
+            </div>
           </div>
-          <div className="logos">
-            <img src={gym}></img>
-            <img src={piscina}></img>
-            <img src={salon}></img>
-            <img src={juegos}></img>
-          </div>
+
           <div className="logosmobile">
             <AutoplaySlider
               organicArrows={true}
@@ -144,25 +152,38 @@ const BlogIndex = ({ data }, location) => {
 
       <div className="section" id="apartamentos">
         <div className="containercarrusel">
-          <div className="planos">
-            <AutoplaySlider
-              organicArrows={false}
-              mobileTouch={true}
-              infinite={true}
-            >
-              <div className="itemcarrusel">
-                <Img fluid={data.plano1.childImageSharp.fluid} />
-              </div>
-              <div className="itemcarrusel">
-                <Img fluid={data.plano2.childImageSharp.fluid} />
-              </div>
-            </AutoplaySlider>
+          <div className="gridplanos">
+            <div className="planos">
+              <AutoplaySlider
+                organicArrows={false}
+                mobileTouch={true}
+                infinite={true}
+              >
+                <div className="itemcarrusel">
+                  <img src={plano1}></img>
+                </div>
+                <div className="itemcarrusel">
+                  <img src={plano1}></img>
+                </div>
+              </AutoplaySlider>
+            </div>
           </div>
-          <p>
-            Completas zonas comunes y apartamentos de 62 m² y 68 m², con dos o
-            tres habitaciones, dos baños, sala, comedor, cocina, zona de ropas,
-            estudio y balcón.
-          </p>
+          <div className="textosplanos">
+            <h4 style={{ textAlign: "center" }}>Completas zonas comunes </h4>
+            <h4
+              style={{
+                textAlign: "center",
+                color: "#FF5527",
+                textTransform: "uppercase",
+              }}
+            >
+              apartamentos de 62 m² y 68 m²,
+            </h4>
+            <p style={{ textAlign: "center" }}>
+              con dos o tres habitaciones, dos baños, sala, comedor, cocina,
+              zona de ropas, estudio y balcón.
+            </p>
+          </div>
         </div>
       </div>
 
@@ -261,12 +282,21 @@ const BlogIndex = ({ data }, location) => {
         </div>
       </div>
 
+      <div className="sectionfooter">
+        <div className="containerfooter">
+          <div>
+            <h2>¿Deseas qué te contactemos?</h2>
+            <p>Por favor diligencia el formulario de contacto</p>
+          </div>
+        </div>
+      </div>
+
       <div className="sectionform" id="footer">
         <div className="container">
           <div className="containerform">
-            <h2>¿Deseas qué te contactemos?</h2>
-            <p>Por favor diligencia el formulario de contacto</p>
-            <hr className="linea"></hr>
+            <div>
+              <Img fixed={data.imgcontact.childImageSharp.fixed} />
+            </div>
             <Userform></Userform>
           </div>
         </div>
@@ -358,8 +388,8 @@ const indexQuery = graphql`
     }
     plano1: file(relativePath: { eq: "precio.png" }) {
       childImageSharp {
-        fluid(maxWidth: 900, quality: 100) {
-          ...GatsbyImageSharpFluid
+        fixed(width: 650, height: 505, quality: 100) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
@@ -409,6 +439,21 @@ const indexQuery = graphql`
       childImageSharp {
         fluid(maxWidth: 175, quality: 100) {
           ...GatsbyImageSharpFluid
+        }
+      }
+    }
+
+    piscina: file(relativePath: { eq: "piscina.png" }) {
+      childImageSharp {
+        fixed(width: 605, height: 505) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    imgcontact: file(relativePath: { eq: "imgcontact.png" }) {
+      childImageSharp {
+        fixed(width: 672, height: 496) {
+          ...GatsbyImageSharpFixed
         }
       }
     }

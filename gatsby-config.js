5b1ac817c67edd7cf54b596c69f62c83f6e91f46
ATSYS <code@atsys.co>
2021-06-03 19:34:41 -0500

    Added gatsby-plugin-remove-serviceworker plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,6 +111,6 @@ module.exports = {
     `gatsby-plugin-netlify`,
     //`gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    //`gatsby-plugin-remove-serviceworker`,
+    `gatsby-plugin-remove-serviceworker`,
   ],
 }
